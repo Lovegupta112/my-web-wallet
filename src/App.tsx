7fe840d899e0371e444762ac860229753f8434ff
lovegupta112 <loveguptaofficials@gmail.com>
2024-08-19 00:34:40 +0530

    fix eslint error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import React from "react";
+import { useState } from "react";
 import { Stack, Button ,Typography } from "@mui/material";
 import WalletComp from "./components/WalletComp";
 
 function App() {
-  const [active, setActive] = React.useState(false);
+  const [active, setActive] = useState(false);
 
   return (
     <>
